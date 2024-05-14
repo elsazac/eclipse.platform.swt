eba2089dbdc42879b689753c95102e574d55b069
shahzaib.ibrahim <shahzaib.ibrahim@yatta.de>
2024-05-14 14:26:13 +0200

    Adapt GetSystemMetrics calls to be DPI dependent
    Writing GetSystemMetrics method in Widget class so that the child
    controls can use it to get System Metrics to be DPI dependent
    
    Contributes to:
    https://github.com/eclipse-platform/eclipse.platform.swt/issues/62 &
    https://github.com/eclipse-platform/eclipse.platform.swt/issues/127

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/ScrollBar.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/ScrollBar.java
@@ -203,10 +203,10 @@ Rectangle getBounds () {
 	if ((style & SWT.HORIZONTAL) != 0) {
 		y = rect.bottom - rect.top;
 		width = rect.right - rect.left;
-		height = OS.GetSystemMetrics (OS.SM_CYHSCROLL);
+		height = getSystemMetrics (OS.SM_CYHSCROLL);
 	} else {
 		x = rect.right - rect.left;
-		width = OS.GetSystemMetrics (OS.SM_CXVSCROLL);
+		width = getSystemMetrics (OS.SM_CXVSCROLL);
 		height = rect.bottom - rect.top;
 	}
 	return new Rectangle (x, y, width, height);
@@ -369,9 +369,9 @@ Point getSizeInPixels () {
 	int width, height;
 	if ((style & SWT.HORIZONTAL) != 0) {
 		width = rect.right - rect.left;
-		height = OS.GetSystemMetrics (OS.SM_CYHSCROLL);
+		height = getSystemMetrics (OS.SM_CYHSCROLL);
 	} else {
-		width = OS.GetSystemMetrics (OS.SM_CXVSCROLL);
+		width = getSystemMetrics (OS.SM_CXVSCROLL);
 		height = rect.bottom - rect.top;
 	}
 	return new Point (width, height);
@@ -472,7 +472,7 @@ Rectangle getThumbTrackBoundsInPixels () {
 	int x = 0, y = 0, width, height;
 	if ((style & SWT.HORIZONTAL) != 0) {
 		OS.GetScrollBarInfo(parent.handle, OS.OBJID_HSCROLL, info);
-		int size = OS.GetSystemMetrics (OS.SM_CYHSCROLL);
+		int size = getSystemMetrics (OS.SM_CYHSCROLL);
 		y = info.rcScrollBar.top;
 		width = info.rcScrollBar.right - info.rcScrollBar.left;
 		height = size;
@@ -485,7 +485,7 @@ Rectangle getThumbTrackBoundsInPixels () {
 		}
 	} else {
 		OS.GetScrollBarInfo(parent.handle, OS.OBJID_VSCROLL, info);
-		int size = OS.GetSystemMetrics (OS.SM_CYVSCROLL);
+		int size = getSystemMetrics (OS.SM_CYVSCROLL);
 		x = info.rcScrollBar.left;
 		width = size;
 		height = info.rcScrollBar.bottom - info.rcScrollBar.top;
